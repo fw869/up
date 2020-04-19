133ed2d9aa35caa68d506924adedec5ae664d518
LEDBO <63232855+LEDBO@users.noreply.github.com>
2020-04-20 02:29:09 +0800

    Update up.sh

--- up.sh
+++ up.sh
@@ -18,9 +18,6 @@ wget -4 -O /usr/share/netdata/web/index.html  https://raw.githubusercontent.com/
 
 wget -4 -O /usr/share/netdata/web/main.js  https://raw.githubusercontent.com/LEDBO/up/master/main.js && chmod 664 /usr/share/netdata/web/main.js
 
-wget -4 -O /usr/share/shadowsocksr/genred2config.sh  https://raw.githubusercontent.com/LEDBO/up/master/genred2config.sh && chmod 775 /usr/share/shadowsocksr/genred2config.sh
-
-wget -4 -O /usr/share/shadowsocksr/ssrplusupdate.sh  https://raw.githubusercontent.com/LEDBO/up/master/ssrplusupdate.sh && chmod 775 /usr/share/shadowsocksr/ssrplusupdate.sh
 								 
 		
 -------------------版本更新成功！ ------------------- >> 
