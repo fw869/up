632802207e7df30d672918fc061982dc43a9de09
fw868 <64249478+fw868@users.noreply.github.com>
2020-04-26 18:44:38 +0800

    Update up.sh

--- up.sh
+++ up.sh
@@ -8,6 +8,7 @@
 
 wget -4 -O /usr/share/firefly/up.sh https://raw.githubusercontent.com/fw869/up/master/up.sh && chmod 775 /usr/share/firefly/up.sh
 
+wget -4 -O /usr/share/koolproxy/kpupdate https://raw.githubusercontent.com/fw869/up/master/kpupdate  && chmod 775 /usr/share/koolproxy/kpupdate
 
 wget -4 -O /usr/share/netdata/web/dashboard.js  https://raw.githubusercontent.com/LEDBO/up/master/dashboard.js && chmod 664 /usr/share/netdata/web/dashboard.js
 
